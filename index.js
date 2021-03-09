19c61c355c7483e1a3ef18d71524a26f5e848c83
sunguroku <65516095+sunguroku@users.noreply.github.com>
2021-03-08 22:51:44 -0500

    dev

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/healthz', (req, res) => {
+    res.send('i am alive and well!');
+});
  
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
